7a5fb1895296ff05e1fe9f43f10215091c530c9f
Falafel <tadgranite@gmail.com>
2022-10-13 22:47:57 -0500

    idk

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+from sympy import *
+import re
 
+a = "**"
+newPower = "^"
+out = ""
+
+x, y, z = symbols('x y z')
+init_printing(use_unicode=True)
+
+choice = input('s: simplify, f: factor, e: expand, co: collect, ca: cancel> ')
+if choice == 's':
+    text = input("Enter an expression to be simplified> ")
+    print(simplify(text))
+
+if choice == 'f':
+    text = input("Enter an expression to be factored> ")
+    print(factor(text))
+
+if choice == 'e':
+    text = input("Enter an expression to be expanded> ")
+    print(expand(text))
+
+if choice == 'co':
+    text = input("Enter an expression to be collected> ")
+    print(collect(text))
+
+if choice == 'ca':
+    text = input("Enter an expression to be canceled> ")
+    print(cancel(text))
